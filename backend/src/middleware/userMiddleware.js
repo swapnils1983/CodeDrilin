46f5ae2ff6af976f5179b3e8c4afe96158acc8a8
Swapnil Sharad Sonawane <155180363+swapnils1983@users.noreply.github.com>
2025-08-22 18:49:21 +0530

    Update userMiddleware.js

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -3,20 +3,20 @@ const User = require('../models/user');
 
 const userMiddleware = async (req, res, next) => {
     try {
-        const { token } = req.cookies;
-        if (!token) {
-            throw new Error("Token missing");
-        }
+        // const { token } = req.cookies;
+        // if (!token) {
+        //     throw new Error("Token missing");
+        // }
 
-        const payload = jwt.verify(token, process.env.JWT_KEY);
-        const { _id } = payload;
+        // const payload = jwt.verify(token, process.env.JWT_KEY);
+        // const { _id } = payload;
 
-        const user = await User.findById(_id);
-        if (!user) {
-            throw new Error("User doesn't exist");
-        }
+        // const user = await User.findById(_id);
+        // if (!user) {
+        //     throw new Error("User doesn't exist");
+        // }
 
-        req.user = user;
+        // req.user = user;
         next();
     } catch (error) {
         res.status(401).send("Authentication failed: " + error.message);
